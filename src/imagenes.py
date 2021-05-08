10c0d4267704685273602dda7e5ef0a8b96e15ed
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-08 13:06:44 -0500

    Imagenes jpg
    Version 1.0.0

--- src/imagenes.py
+++ src/imagenes.py
@@ -6,3 +6,27 @@ Python: 3.9.1
 Fecha: 08 Mayo 2020
 """
 
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from lxml import html
+import os
+
+def main():
+    os.system('cls')
+    url = 'https://www.uanl.mx/enlinea'
+    print(datetime.now(), "\033[0;32m [INFO] Buscando... %s \033[0;0m" % url)
+    response = requests.get(url)
+    bs = BeautifulSoup(response.text, 'lxml')
+    parsed_body = html.fromstring(response.text)
+    src = parsed_body.xpath('//img/@src')
+    print(datetime.now(), "\033[0;32m [INFO] %s imagenes encontradas \033[0;0m" % len(src))
+    imagenes = bs.find_all("img",src=True)
+    imagen_src = [img['src'] for img in imagenes]
+    imagen = [img for img in imagen_src if img.endswith('.jpg')]          
+    for enlace in imagen:
+        print(datetime.now(), "\033[0;32m [INFO] %s \033[0;0m" % enlace)
+
+if __name__ == '__main__':
+    main()
+
