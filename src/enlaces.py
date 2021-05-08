66f47d8ec5749029dec2a8d68928f9864295995b
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-08 12:12:00 -0500

    Obtención de la lista de enlaces que contiene una página Web
    Version 1.0.0

--- src/enlaces.py
+++ src/enlaces.py
@@ -1,8 +1,32 @@
 """
-Uso: Web Scraping (Texto)
+Uso: Obtención de la lista de enlaces que contiene una página Web
 Creador: Andrés Hernández Mata
 Version: 1.0.0
 Python: 3.9.1
 Fecha: 08 Mayo 2020
 """
 
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from lxml import html
+import os
+
+def main():
+    os.system('cls')
+    url = 'https://www.uanl.mx/enlinea'
+    print(datetime.now(), "\033[0;32m [INFO] Buscando... %s \033[0;0m" % url)
+    response = requests.get(url)
+    bs = BeautifulSoup(response.text, 'lxml')
+    parsed_body = html.fromstring(response.text)
+    href = parsed_body.xpath('//a/@href')
+    print(datetime.now(), "\033[0;32m [INFO] %s enlaces encontrados \033[0;0m" % len(href))
+    enlaces = bs.find_all("a",href=True)
+    for enlace in enlaces:
+        direcciones = [a['href'] for a in enlaces]
+    for link in direcciones:
+        if not link == '#':
+            print(datetime.now(), "\033[0;32m [INFO] %s \033[0;0m" % link)
+
+if __name__ == '__main__':
+    main()
