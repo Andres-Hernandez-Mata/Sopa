d707a7742877df6378d215b421718a59a92aef8c
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-15 14:12:01 -0500

    Obtener metadata

--- src/mi_exif.py
+++ src/mi_exif.py
@@ -10,13 +10,12 @@ from exif import Image
 import os
 from datetime import datetime
 
-os.system("cls")
+clear = lambda: os.system("cls" if os.name=="nt" else "clear")
 
-imagenes = "piramide.jpg"
+clear()
 
-with open(imagenes, "rb+") as imagen:
+with open("tosticentro.jpg", "rb") as imagen:
     img = Image(imagen)
-    print(dir(img))
     if img.has_exif:
         print(datetime.now(), "\033[0;32m [INFO] Metadata \033[0;0m")        
         if img.model:
@@ -30,23 +29,24 @@ with open(imagenes, "rb+") as imagen:
         if img.gps_longitude:
             print(datetime.now(), "\033[0;32m [INFO] GPS longitude > %s %s \033[0;0m" % (str(img.gps_longitude), img.gps_longitude_ref))    
     img.model = "GoPro"
-    img.mensaje = "Yo estuve ahi"    
-    imagen.close()
+    img.mensaje = "Yo estuve ahi"
     
-with open(imagenes, "wb") as imagen:
-    imagen.write(img.get_file())
+with open("tosticentro.jpg", "wb") as new_imagen:    
+    new_imagen.write(img.get_file())
 
+print("--------------------------------------------------------------------------------------")
 print(datetime.now(), "\033[0;31m [INFO] Modificar atributo \033[0;0m")
 print(datetime.now(), "\033[0;32m [INFO] Model > %s \033[0;0m" % img.model)    
 if img.mensaje:
     print(datetime.now(), "\033[0;31m [INFO] Nuevo atributo \033[0;0m")
-    print(datetime.now(), "\033[0;32m [INFO] Mensaje > %s \033[0;0m" % img.mensaje)        
+    print(datetime.now(), "\033[0;32m [INFO] Mensaje > %s \033[0;0m" % img.mensaje)
 
+print("--------------------------------------------------------------------------------------")
 print(datetime.now(), "\033[0;31m [INFO] Borrando atributo \033[0;0m")
-img.delete("mensaje")
+del img.mensaje
 print(datetime.now(), "\033[0;32m [INFO] Atributo mensaje > %s \033[0;0m" % img.get('mensaje', 'Not found'))
 
 print(datetime.now(), "\033[0;33m [INFO] Guardando imagen... \033[0;0m")
-imagen.close()
+new_imagen.close()
 
     
